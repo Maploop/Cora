8c637b7a0758434f835abdbda6b718d6888c98b1
Maploop <sweatypvpmc@gmail.com>
2024-01-05 20:13:22 +0330

    Vulkan renderer with strcuture to support more than vulkan in the future

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -5,6 +5,8 @@
 #include "core/cmemory.h"
 #include "core/event.h"
 #include "core/input.h"
+#include "core/clock.h"
+#include "renderer/renderer_frontend.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -13,6 +15,7 @@ typedef struct application_state {
     platform_state platform;
     i16 width;
     i16 height;
+    clock clock;
     f64 last_time;
 } application_state;
 
@@ -66,6 +69,11 @@ b8 application_create(game* game_inst) {
         return FALSE;
     }
 
+    if (!renderer_initialize(game_inst->app_config.name, &app_state.platform)) {
+        CFATAL("Failed to initialize renderer. Aborting application.");
+        return FALSE;
+    }
+
     if (!app_state.game_inst->initialize(app_state.game_inst)) {
         CFATAL("Game failed to initialize");
         return FALSE;
@@ -78,26 +86,62 @@ b8 application_create(game* game_inst) {
 }
 
 b8 application_run() {
+    clock_start(&app_state.clock);
+    clock_update(&app_state.clock);
+    app_state.last_time = app_state.clock.elapsed;
+    f64 running_time = 0;
+    u8 frame_count = 0;
+    f64 target_frame_seconds = 1.0f / 60;
+
     CINFO(get_memory_usage_str());
 
     while (app_state.is_running) {
+        clock_update(&app_state.clock);
+        f64 current_time = app_state.clock.elapsed;
+        f64 delta = (current_time - app_state.last_time);
+        f64 frame_start_time = platform_get_absolute_time();
+
         if (!platform_pump_messages(&app_state.platform)) {
             app_state.is_running = FALSE;
         }
         if (!app_state.is_suspended) {
-            if (!app_state.game_inst->update(app_state.game_inst, (f32)0)) {
+            if (!app_state.game_inst->update(app_state.game_inst, (f32) delta)) {
                 CFATAL("Game update routine failed, shutdown in progress...");
                 app_state.is_running = FALSE;
                 break;
             }
 
-            if (!app_state.game_inst->render(app_state.game_inst, (f32)0)) {
+            if (!app_state.game_inst->render(app_state.game_inst, (f32) delta)) {
                 CFATAL("Game render routine failed, shutdown in progress...");
                 app_state.is_running = FALSE;
                 break;
             }
 
-            input_update(0);
+            // TODO: refactor packet creation
+            render_packet packet;
+            packet.delta_time = delta;
+            renderer_draw_frame(&packet);
+
+            f64 frame_end_time = platform_get_absolute_time();
+            f64 frame_elapsed_time = frame_end_time - frame_start_time;
+            running_time += frame_elapsed_time;
+            f64 remaining_seconds = target_frame_seconds - frame_elapsed_time;
+
+            if (remaining_seconds > 0) {
+                u64 remaining_ms = (remaining_seconds * 1000);
+                
+                // NOTE: this is hardcoded to false for now!
+                b8 limit_frames = FALSE;
+
+                if (remaining_ms > 0 && limit_frames) {
+                    platform_sleep(remaining_ms - 1);
+                }
+
+                frame_count++;
+            }
+
+            input_update(delta);
+            app_state.last_time = current_time;
         }
     }
 
@@ -109,6 +153,8 @@ b8 application_run() {
     event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
     event_shutdown();
     input_shutdown();
+
+    renderer_shutdown();
 
     platfrom_shutdown(&app_state.platform);
 
